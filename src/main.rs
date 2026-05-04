df49f1064f9111ca25e12bf54cd39c58861f13c3
Roger Chau <54104821+roger-chau@users.noreply.github.com>
2026-05-04 17:12:34 -0600

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // Image
+    let image_width: i32 = 256;
+    let image_height: i32 = 256;
+
+    // Render
+    println!("P3\n {} {} \n255\n", image_width, image_height);
+
+    for j in 0..image_height {
+        eprintln!("\rScanlines remaining: {}", image_height - j);
+        for i in 0..image_width {
+            let r = (i as f64) / ((image_height-1) as f64);
+            let g = (j as f64) / ((image_width-1) as f64);
+            let b = 0.0;
+
+            let ir: i32 = (255.999 * r) as i32;
+            let ig: i32 = (255.999 * g) as i32;
+            let ib: i32 = (255.999 * b) as i32;
+
+            println!("{ir} {ig} {ib}");
+        }
+        eprintln!("\rDone.                 \n");
+    }
 }
